27eb5b31f73276dc563e3b206592235080e47764
kawalees <kawalees.art@gmail.com>
2026-06-18 11:11:01 +0300

    Localize docs pages for Arabic RTL rendering

---
+++ docs/assets/phone-DB2s6Whx.js
@@ -0,0 +1,11 @@
+import{c as a}from"./index-NiaiG8b9.js";/**
+ * @license lucide-react v1.20.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M9 17H7A5 5 0 0 1 7 7h2",key:"8i5ue5"}],["path",{d:"M15 7h2a5 5 0 1 1 0 10h-2",key:"1b9ql8"}],["line",{x1:"8",x2:"16",y1:"12",y2:"12",key:"1jonct"}]],c=a("link-2",o);/**
+ * @license lucide-react v1.20.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M13.832 16.568a1 1 0 0 0 1.213-.303l.355-.465A2 2 0 0 1 17 15h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2A18 18 0 0 1 2 4a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-.8 1.6l-.468.351a1 1 0 0 0-.292 1.233 14 14 0 0 0 6.392 6.384",key:"9njp5v"}]],t=a("phone",e);export{c as L,t as P};
